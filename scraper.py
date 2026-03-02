4c1451078d0bf2e4c9baae4b1918d1673ebc4710
Shivam-Shukla0 <shivshukla00846@gmail.com>
2026-03-02 20:50:54 +0530

    fixes bugs in selenium

--- scraper.py
+++ scraper.py
@@ -9,7 +9,6 @@ def scrape_page(website_link):
     if not website_link.startswith("http") and not website_link.startswith("https"):
         website_link = "https://" + website_link
 
-    # browser silently chalao
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -17,30 +16,25 @@ def scrape_page(website_link):
     driver = webdriver.Chrome(options=options)
     driver.get(website_link)
 
-    # JS load hone ka wait karo
     time.sleep(3)
-
     html = driver.page_source
     driver.quit()
 
     filter_html_page = BeautifulSoup(html, "html.parser")
 
-    # title
     if filter_html_page.title and filter_html_page.title.string:
         print(filter_html_page.title.string.strip())
     else:
         print("")
 
-    # script style hatao
     elements = filter_html_page(["script", "style"])
     while len(elements) > 0:
         elements[0].decompose()
         elements = filter_html_page(["script", "style"])
 
-    # body text
     body_tag_content = filter_html_page.find("body")
     if body_tag_content:
-        text = body_tag_content.get_text()
+        text = body_tag_content.get_text(separator="\n")
         lines = text.split("\n")
         i = 0
         while i < len(lines):
@@ -49,9 +43,11 @@ def scrape_page(website_link):
                 print(cleaned_content)
             i += 1
 
-    # links
-    for anchor_tag_in_page in filter_html_page.find_all("a", href=True):
-        print(urljoin(website_link, anchor_tag_in_page["href"]))
+    links = filter_html_page.find_all("a", href=True)
+    i = 0
+    while i < len(links):
+        print(urljoin(website_link, links[i]["href"]))
+        i += 1
 
 
 if __name__ == "__main__":
